a431e0621596ceff73df0ee021b2ec07977c67f0
nicomp42 <nicholdw@ucmail.uc.edu>
2025-03-12 09:30:16 -0400

    Add project files.

---
+++ IS3050 Spring 2025 Assignment08/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS3050_Spring_2025_Assignment08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        /// <summary>
+        /// Validate a password
+        /// </summary>
+        /// <param name="password">The string to validate</param>
+        /// <returns>True if valid, False otherwise</returns>
+        static bool ValidatePassword(String password) {
+            Boolean status = true;      // Assume it's valid
+
+
+            return status;
+        }
+    }
+}
