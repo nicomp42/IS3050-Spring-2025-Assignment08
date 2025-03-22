e6e370604255c06a837361d6e03890308653397b
nicomp42 <nicholdw@ucmail.uc.edu>
2025-03-22 11:57:12 -0400

    Added ValidatePassword method

--- IS3050 Spring 2025 Assignment08/Program.cs
+++ IS3050 Spring 2025 Assignment08/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * IS3050 Spring 2025 Assignment 08
+ * Created by Bill Nicholson
+ * Edited by
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
@@ -11,16 +17,40 @@ namespace IS3050_Spring_2025_Assignment08
     {
         static void Main(string[] args)
         {
+
         }
         /// <summary>
-        /// Validate a password
+        /// Validate a password.
         /// </summary>
         /// <param name="password">The string to validate</param>
         /// <returns>True if valid, False otherwise</returns>
-        static bool ValidatePassword(String password) {
-            Boolean status = true;      // Assume it's valid
+        static bool ValidatePassword(String password)
+        {
+            Boolean status = true;     // Assume it's valid
 
+            if (password == null || password.Length < 10) {
+                return false; // Must be at least 10 characters long
+            }
 
+            bool hasSpecial = false;
+            bool hasLower = false;
+            bool hasUpper = false;
+
+            foreach (char c in password) {
+                if ("~!@#$%^&*()".Contains(c)) {
+                    hasSpecial = true;
+                }
+                else if (char.IsLower(c)) {
+                    hasLower = true;
+                }
+                else if (char.IsUpper(c)) {
+                    hasUpper = true;
+                }
+            }
+
+            if (!hasSpecial || !hasLower || !hasUpper) {
+                status = false;
+            }
             return status;
         }
     }
